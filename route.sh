7f7c368481b416bce630a4cd93542dbdd8d4e99f
Devsoft <by-JohnChen@users.noreply.github.com>
2021-10-26 23:19:57 +0800

    Update route.sh

--- route.sh
+++ route.sh
@@ -2,5 +2,5 @@
 gw=$(ifconfig|grep P-t-P|cut -d " " -f 14|cut -d ":" -f 2) #get PPPOE default Gateware from interface
 for i in $gw;
 do
-    route add default gw $i
+    route add default gw $i #add the gateware into route table.
 done
