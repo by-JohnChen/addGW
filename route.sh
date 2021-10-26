52a44b75e9e3d5bfd43ffbe56a6ed350b0862bfd
Devsoft <by-JohnChen@users.noreply.github.com>
2021-10-26 23:28:40 +0800

    Update route.sh
    添加注释

--- route.sh
+++ route.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gw=$(ifconfig|grep P-t-P|cut -d " " -f 14|cut -d ":" -f 2) #get PPPOE default Gateware from interface
+gw=$(ifconfig|grep P-t-P|awk '{print $3}'|cut -d ":" -f 2) #get PPPOE default Gateware from interface
 for i in $gw;
 do
     route add default gw $i #add the gateware into route table.
